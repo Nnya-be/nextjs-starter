b84f4e618ce3bdbf0bd42c4d9526f6682090f333
Chamamme <nnyabe.chamamme@amalitech.com>
2025-08-15 12:39:48 +0000

    add: error fixed

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { LoggerService } from './logger/logger.service';
-// import {AllExceptionsFilter} from './all-exceptions.filter';
+import { LoggerService } from './logger/logger.service';
+import {AllExceptionsFilter} from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const { httpAdapter } = app.get(HttpAdapterHost)
-  // app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter))
 
   // app.useLogger(app.get(LoggerService)), {bufferLogs: true}
   app.setGlobalPrefix('api/v1');
